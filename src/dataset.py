0f42fe63ea8112ba329f0735600cdc62c46f4373
CheHsuan <exas60807@gmail.com>
2017-04-17 20:41:46 +0800

    download and extract the dataset

--- src/dataset.py
+++ src/dataset.py
@@ -30,6 +30,7 @@ def reformat(dataset, labels):
     return dataset, labels
 
 def open_cifar10_dataset(start, length):
+    maybe_download_and_extract() 
     data_dir = '/home/rche/data/cifar-10-batches-py'
     return load_and_preprocess_input(data_dir, start, length)
 
@@ -107,3 +108,23 @@ def load_and_preprocess_input(dataset_dir, start, length):
             vldte_all_labels,
             tst_all_data,
             tst_all_labels)
+
+def maybe_download_and_extract():
+    """Download and extract the tarball from Alex's website."""
+    DATA_URL = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+    dest_directory = '/home/rche/data/'
+    if not os.path.exists(dest_directory):
+        os.makedirs(dest_directory)
+    filename = DATA_URL.split('/')[-1]
+    filepath = os.path.join(dest_directory, filename)
+    if not os.path.exists(filepath):
+        def _progress(count, block_size, total_size):
+            sys.stdout.write('\r>> Downloading %s %.1f%%' % (filename, float(count * block_size) / float(total_size) * 100.0))
+            sys.stdout.flush()
+        filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+        print()
+        statinfo = os.stat(filepath)
+        print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
+    extracted_dir_path = os.path.join(dest_directory, 'cifar-10-batches-py')
+    if not os.path.exists(extracted_dir_path):
+        tarfile.open(filepath, 'r:gz').extractall(dest_directory)
